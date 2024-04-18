bcd433ccb36d94d9564f1268e8b490d645dd3b40
mahuixian <mhx@DESKTOP-I3GDQNQ.localdomain>
2024-04-18 17:29:25 +0800

    renew mytokenizer

--- mytokenizer.py
+++ mytokenizer.py
@@ -10,27 +10,28 @@ from tokenizers import (
     trainers,
 )
 import json
+import os
+from pathlib import Path
 
 
-def dataloader(datapaths):
-    datasets = DatasetDict()
-    
-    for datapath in datapaths:
-        dataset = load_dataset('json', data_dir=datapath)
-        datasets = datasets.update(dataset)
-    
+def dataloader(data_dir):
+    data_files = []
+    for file in os.listdir(data_dir):
+        if file.endswith('.json'):
+            data_files.append(os.path.join(data_dir, file))
+    datasets = load_dataset('json', data_files=data_files)
     return datasets
         
 
 
 def batch_iterator(datasets, batch_size=10000):
     for i in range(0, len(datasets), batch_size):
-        items = [info['instruction'] + " " + info['input'] + " " + info['output'] for info in datasets[i:i+batch_size]]
+        items = [instruction+' '+input+' '+output for instruction, input, output in zip(datasets[i:i+batch_size]["instruction"], datasets[i:i+batch_size]["input"], datasets[i:i+batch_size]["output"])]
         yield items
     
 
 
-def train_tokenizer(file_path):
+def train_tokenizer(data_dir, tokenizer_path):
     tokenizer = Tokenizer(models.BPE(unk_token="[UNK]"))
     normalizer = normalizers.Sequence([normalizers.NFD(), normalizers.Lowercase(), normalizers.StripAccents(), normalizers.Strip()])
     # # #标点符号，空白，数字
@@ -42,9 +43,9 @@ def train_tokenizer(file_path):
     tokenizer.model = model
     tokenizer.decoder = decoder
     
-    special_tokens = ["[CLS]", "[SEP]", "[PAD]", "[MASK]", "<s>", "</s>", "<t>", "</t>"]
+    special_tokens = ["[UNK]", "[CLS]", "[SEP]", "[PAD]", "[MASK]", "<s>", "</s>", "<t>", "</t>"]
     trainer = trainers.BpeTrainer(special_tokens=special_tokens, vocab_size=54000)
-    tokenizer.train_from_iterator(batch_iterator(file_path), trainer=trainer)
+    tokenizer.train_from_iterator(batch_iterator(dataloader(data_dir)), trainer=trainer)
     
     # #字节级 BPE 可能在生成的令牌中包括空白。如果您不希望偏移量包含这些空格，那么必须使用这个 PostProcessor。
     post_processor = processors.TemplateProcessing(single="[CLS] $A [SEP]",
@@ -55,7 +56,7 @@ def train_tokenizer(file_path):
     ],)
 
     tokenizer.post_processor = post_processor
-    tokenizer.save("./mytokenizer.json")
+    tokenizer.save(tokenizer_path)
     
 
 def test_tokenizer(tokenizer_path, input):
@@ -64,8 +65,11 @@ def test_tokenizer(tokenizer_path, input):
     
 
 if __name__ == '__main__':
-    train_tokenizer('all.json')
-    test_tokenizer('今天天气真好啊！')
+    data_dir = './data'
+    
+    tokenizer_path = './tokenizer.json'
+    train_tokenizer(data_dir, tokenizer_path)
+    test_tokenizer(tokenizer_path, '今天天气真好啊！')
 
 
             
